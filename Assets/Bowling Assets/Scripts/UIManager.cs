8eeb4c3251285b99d8878c15cde6a71106fe6ca6
XtraReal <149747643+XtraReal@users.noreply.github.com>
2023-11-09 16:53:42 -0500

    Added UI Manager 10-1102

---
+++ Assets/Bowling Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Transform frameHolder;
+    [SerializeField] private GameObject messageUIStrike;
+    [SerializeField] private GameObject messageUISpare;
+    [SerializeField] private GameObject gameOverUI;
+    [SerializeField] private TMP_Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
